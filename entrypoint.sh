4a093cbf712aee428024c172edce74ee9c92a922
Okan <sahinerokan@gmail.com>
2017-07-20 19:02:13 +0300

    fixed privileges on entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,5 +6,5 @@ export POD_NETMASK=$(ip a | grep inet | grep eth0 | awk '{print $2}'| cut -d "/"
 export POD_DEFGW=$(ip route | grep default | awk '{print $3}')
 export POD_UID=$(id -u)
 
-nginx -g "daemon off;"
+/usr/local/openresty/nginx/sbin/nginx -g "daemon off;"
 
